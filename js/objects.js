37341d8bd576dd6fab73d066c2906fd36c8f7e6c
Unknown <dragonbotguardian@gmail.com>
2016-09-19 11:54:08 -0600

    created a robot object literal.

--- js/objects.js
+++ js/objects.js
@@ -1,1 +1,25 @@
 //Objects lesson.
+var robot = {
+    fuel: 100,
+    weapon: "laser",
+    speed: 10,
+    strength: 5,
+    armor: "light",
+
+    addFuel: function (tank) {
+        this.fuel += tank;
+    },
+
+    upgradeArmor: function (change) {
+        this.armor = change;
+    },
+
+    changeWeapon: function (change) {
+        this.weapon = change;
+    },
+}
+
+robot.speed += parseInt(prompt("Increase robot speed."));
+alert("Our robot now has a speed of " + robot.speed);
+robot.upgradeArmor(prompt("What should we upgrade the armor to?"));
+alert("Our robot now has " + robot.armor + " armor.");
